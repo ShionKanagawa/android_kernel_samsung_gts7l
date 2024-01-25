a03491dfc654c3128f653b54ded1473adc518471
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2024-01-25 23:48:28 +0800

    BACKPORT: mm: multi-gen LRU: Move lru_gen_add_mm() out of IRQ-off region
    lru_gen_add_mm() has been added within an IRQ-off region in the commit
    mentioned below.  The other invocations of lru_gen_add_mm() are not within
    an IRQ-off region.
    
    The invocation within IRQ-off region is problematic on PREEMPT_RT because
    the function is using a spin_lock_t which must not be used within
    IRQ-disabled regions.
    
    The other invocations of lru_gen_add_mm() occur while
    task_struct::alloc_lock is acquired.  Move lru_gen_add_mm() after
    interrupts are enabled and before task_unlock().
    
    Bug: 254441685
    Link: https://lkml.kernel.org/r/20221026134830.711887-1-bigeasy@linutronix.de
    Fixes: bd74fdaea1460 ("mm: multi-gen LRU: support page table walks")
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Acked-by: Yu Zhao <yuzhao@google.com>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Cc: "Eric W . Biederman" <ebiederm@xmission.com>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- fs/exec.c
+++ fs/exec.c
@@ -1059,11 +1059,11 @@ static int exec_mmap(struct mm_struct *mm)
 	task_lock(tsk);
 	active_mm = tsk->active_mm;
 	tsk->mm = mm;
-	lru_gen_add_mm(mm);
 	tsk->active_mm = mm;
 	activate_mm(active_mm, mm);
 	lru_gen_use_mm(mm);
 	tsk->mm->vmacache_seqnum = 0;
+	lru_gen_add_mm(mm);
 	vmacache_flush(tsk);
 #ifdef CONFIG_KDP_CRED
 	if(rkp_cred_enable)
